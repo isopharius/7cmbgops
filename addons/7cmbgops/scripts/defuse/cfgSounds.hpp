2a008ecba26242310c87c24985b3a550f367db40
isopharius <isopharius@gmail.com>
2016-08-18 18:14:45 -0400

    move sounds

---
+++ addons/7cmbgops/scripts/defuse/cfgSounds.hpp
@@ -0,0 +1,24 @@
+class button_click
+{
+	name = "";
+	sound[] = {"\7cmbg_sounds\effects\click.ogg", 0.8, 1};
+	titles[] = {};
+};
+class button_close
+{
+	name = "";
+	sound[] = {"\7cmbg_sounds\effects\close.ogg", 0.8, 1};
+	titles[] = {};
+};
+class button_wrong
+{
+	name = "";
+	sound[] = {"\7cmbg_sounds\effects\wrong.ogg", 0.8, 1};
+	titles[] = {};
+};
+class wire_cut
+{
+	name = "";
+	sound[] = {"\7cmbg_sounds\effects\cutting.ogg", 1, 1};
+	titles[] = {};
+};
